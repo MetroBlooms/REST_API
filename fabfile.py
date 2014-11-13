2a9ad4f37f767339862d94f365bd111c6905e9bc
gms <gms@umn.edu>
2014-11-13 11:17:31 -0600

    Comment fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,12 +4,14 @@ from contextlib import contextmanager
 env.directory = '~/development/python/rest-api'
 env.activate = 'source ' + env.directory + '/venv/bin/activate' # use virtual environment to execute commands
 
+# set context manager to use virtualenv
 @contextmanager
 def virtualenv():
     with cd(env.directory):
         with prefix(env.activate):
             yield
 
+# set up environment prior to dev
 def prepare():
      with virtualenv():
         local('git pull')
@@ -19,11 +21,13 @@ def prepare():
         # ensure correct python from virtual environment is being used
         local('which python')
 
+# create db structure from models
 def db_create():
      with virtualenv():
         local('which python')
         local('./db_create.py')
 
+# make API available
 def api():
     with virtualenv():
         local('which python')
